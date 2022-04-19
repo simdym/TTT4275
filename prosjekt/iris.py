6c3f518b2a8c65ea379cdf79de11391773ebc64a
Simen Dymbe <simen.dymbe@gmail.com>
2022-04-19 19:43:44 +0200

    added function to split data set and histogram plotting

--- prosjekt/iris.py
+++ prosjekt/iris.py
@@ -36,6 +36,40 @@ def getFeatureAndLabelMatrix(file_array, class_array):
 
     return feature_matrix, label_matrix
 
+
+def split_dataset(x_matrix, t_matrix, split_index):
+    """
+    Splits data set into two new data sets. Order of label and feature amtricies must be matched.
+
+    :param x_matrix: Feature matrix from orginal data set
+    :param t_matrix: Label matrix from orginal data set
+    :param split_index: Amount of samples from each class popped from orignal data set into new data set
+    :return: feature and label matrix with split_index amount of data points removed from each class,
+            and feature and label matrix with split_index amount of data points
+    """
+    #array with class
+    classes = getActualClass(t_matrix, axis=1)
+
+    new_x_matrix = []
+    new_t_matrix = []
+    all_class_indicies = np.array([], dtype=int)
+    for current_class in range(max(classes) + 1):
+        class_indicies = np.where(classes == current_class)[0]
+        all_class_indicies = np.append(all_class_indicies, class_indicies[:split_index])
+        for i in range(split_index):
+            #feature
+            new_x_matrix.append(x_matrix[class_indicies[i]])
+
+            #label
+            new_t_matrix.append(t_matrix[class_indicies[i]])
+    x_matrix = np.delete(x_matrix, all_class_indicies, axis=0)
+    t_matrix = np.delete(t_matrix, all_class_indicies, axis=0)
+    return x_matrix, t_matrix, np.array(new_x_matrix), np.array(new_t_matrix)
+
+
+def remove_feature(x_matrix, feature_index):
+    return np.delete(x_matrix, feature_index, axis=1)
+
 def z(W, x):
     """
     Multiplies weight matrix W with feature vector x
@@ -66,18 +100,6 @@ def g(W, x):
     :return: product of weight matrix W and feature vector x restricted to be between 0 and 1 with sigmoid function
     """
     return sigmoid(z(W, x))
-
-
-def create_W(classes, features):
-    """
-    Initializes weight vector with all zeros and shape (classes X features + 1)
-
-    :param classes: amount of classes
-    :param features: amount of features
-    :return: weight matrix with only zeros
-    """
-    W = np.zeros(shape=(classes, features + 1))
-    return W
 
 
 def MSE_grad_g(W, x, t):
@@ -192,7 +214,8 @@ def shuffle(x_matrix, t_matrix):
     return x_matrix, t_matrix
 
 
-def shuffle_in_unison(a_matrix, b_matrix):
+def shuffle_in_unison(a_matrix, b_matrix, seed=100):
+    np.random.seed(seed)
     rng_state = np.random.get_state()
     np.random.shuffle(a_matrix)
     np.random.set_state(rng_state)
@@ -200,9 +223,8 @@ def shuffle_in_unison(a_matrix, b_matrix):
     return a_matrix, b_matrix
 
 
-def plot_incorrect_and_correct(W, x_matrix, t_matrix, g_matrix=None):
-    if g_matrix==None:
-        g_matrix = np.transpose(g(W, np.transpose(x_matrix)))
+def plot_incorrect_and_correct(W, x_matrix, t_matrix, class_names, feature_names):
+    g_matrix = np.transpose(g(W, np.transpose(x_matrix)))
     actual_classes = getActualClass(t_matrix, axis=1)
     predicted_classes = getPredictedClass(g_matrix, axis=1)
 
@@ -215,6 +237,9 @@ def plot_incorrect_and_correct(W, x_matrix, t_matrix, g_matrix=None):
 
         color = colors[actual]
 
+        for i in range(5-len(x)):
+            x = np.append(x, 0)
+        print(x)
         plt.subplot(2, 1, 1)
         plt.plot(x[1], x[2], marker=sign, c=color)
         plt.subplot(2, 1, 2)
@@ -228,7 +253,7 @@ def get_confusion_matrix(g_matrix, t_matrix):
     return confusion_matrix(actual_classes, predicted_classes)
 
 
-def plot_confusion_matrix(g_matrix, t_matrix):
+def plot_confusion_matrix(g_matrix, t_matrix, class_names):
     confusion_matrix = get_confusion_matrix(g_matrix, t_matrix)
 
     print(confusion_matrix)
@@ -239,38 +264,153 @@ def plot_confusion_matrix(g_matrix, t_matrix):
     ax.set_xlabel('\nPredicted Values')
     ax.set_ylabel('Actual Values ')
 
-    ## Ticket labels - List must be in alphabetical order
-    ax.xaxis.set_ticklabels(['Setosa', 'Versicolor', 'Virginia'])
-    ax.yaxis.set_ticklabels(['Setosa', 'Versicolor', 'Virginia'])
+    ax.xaxis.set_ticklabels(class_names)
+    ax.yaxis.set_ticklabels(class_names)
 
-    ## Display the visualization of the Confusion Matrix.
+    plt.show()
+
+
+def plot_histogram(x_matrix, t_matrix, class_names, feature_names, hist_range, step):
+    classes = getActualClass(t_matrix, axis=1)
+
+    for feature in range(1, len(x_matrix[0])):
+        fig, ax = plt.subplots()
+        ax.set_title(feature_names[feature-1])
+        for current_class in range(max(classes)+1):
+            class_indicies = np.where(classes == current_class)[0]
+            feature_with_class = x_matrix[class_indicies]
+            ax.hist(feature_with_class[:, feature], alpha=0.5, stacked=True, bins=np.arange(hist_range[0], hist_range[1], step), label=class_names[current_class])
+    plt.legend()
+    plt.show()
+
+    # Show plot
     plt.show()
 
 
 def main():
+    #Set-up
+    class_names = ['Setosa', 'Versicolor', 'Virginia']
+    feature_names = ['Sepal length', 'Sepal width', 'Petal length', 'Petal width']
+
     file_array = [f'class_{i}' for i in range(1, 3 + 1)]
     class_array = [i for i in range(len(file_array))]
-    #class_array = [1,2,3]
+
+    x_matrix, t_matrix = getFeatureAndLabelMatrix(file_array, class_array)
+
+    #Task 1 - a)
+
+    training_x_matrix, training_t_matrix, test_x_matrix, test_t_matrix \
+        = split_dataset(x_matrix, t_matrix, 20)
+
+    print("Training set:", len(training_x_matrix), len(training_t_matrix))
+    print("Test set:", len(test_x_matrix), len(test_t_matrix))
+    input("Press any key to contiune to b)")
+
+    # - b)
+
+    num_of_iterations = 1000
+    step_size = 0.01
+
+    print("Training linear classifer with alpha =", step_size, "and", num_of_iterations, "iterations.")
+
+    classes = len(t_matrix[0])
+    features = len(x_matrix[0]) - 1
+    W_array = [np.zeros(shape=(classes, features + 1))]
+    for i in range(num_of_iterations):
+        W_array = np.append(W_array, train_linear_classifier_iteration(W_array[-1],
+                                                                       training_x_matrix, training_t_matrix,
+                                                                       alpha=step_size), axis=0)
+
+    print("Linear classifer trained.")
+    print("For the test set we get MSE and error rate:")
+
+    W_array = np.array(W_array)
+    mse_array = []
+    error_rate_array = []
+    for W in W_array:
+        test_g_matrix = np.transpose(g(W, np.transpose(test_x_matrix)))
+        mse_array.append(MSE(test_g_matrix, test_t_matrix))
+        error_rate_array.append(error_rate(test_g_matrix, test_t_matrix))
+
+    plt.subplot(2, 1, 1)
+    plt.title("MSE")
+    plt.plot(np.arange(len(mse_array)) / 10, mse_array)
+    plt.subplot(2, 1, 2)
+    plt.title("Error rate")
+    plt.plot(np.arange(len(mse_array)) / 10, error_rate_array)
+    plt.show()
+
+    print("and confusion matrix:")
+
+    test_g_matrix = np.transpose(g(W_array[-1], np.transpose(test_x_matrix)))
+    plot_confusion_matrix(test_g_matrix, test_t_matrix, class_names)
+
+    input("Press any key to continue to c) and see the same for training set")
+
+    # - c)
+    print("For the training set we get MSE and error rate:")
+
+    W_array = np.array(W_array)
+    mse_array = []
+    error_rate_array = []
+    for W in W_array:
+        training_g_matrix = np.transpose(g(W, np.transpose(training_x_matrix)))
+        mse_array.append(MSE(training_g_matrix, training_t_matrix))
+        error_rate_array.append(error_rate(training_g_matrix, training_t_matrix))
+
+    plt.subplot(2, 1, 1)
+    plt.title("MSE")
+    plt.plot(np.arange(len(mse_array)) / 10, mse_array)
+    plt.subplot(2, 1, 2)
+    plt.title("Error rate")
+    plt.plot(np.arange(len(mse_array)) / 10, error_rate_array)
+    plt.show()
+
+    print("and confusion matrix:")
+
+    training_g_matrix = np.transpose(g(W_array[-1], np.transpose(training_x_matrix)))
+    plot_confusion_matrix(training_g_matrix, training_t_matrix, class_names)
+
+    # - d)
+    print("Thoughts?")
+
+
+    #2 - a)
+    print("Plotting histograms")
+    plot_histogram(x_matrix, t_matrix, class_names, feature_names, hist_range=[0, 10], step=0.2)
+
+    print("Most overlap in sepal width")
+
+    return
+
+    class_names = ['Setosa', 'Versicolor', 'Virginia']
+    feature_names = ['Sepal length', 'Sepal width', 'Petal length', 'Petal width']
+
+    file_array = [f'class_{i}' for i in range(1, 3 + 1)]
+    class_array = [i for i in range(len(file_array))]
 
     x_matrix, t_matrix = getFeatureAndLabelMatrix(file_array, class_array)
 
     print("Retrived matricies")
 
-    x_matrix, t_matrix = shuffle_in_unison(x_matrix, t_matrix)
+    training_x_matrix, training_t_matrix, test_x_matrix, test_t_matrix = split_dataset(x_matrix, t_matrix, 20)
+
+
+    training_x_matrix, training_t_matrix = shuffle_in_unison(training_x_matrix, training_t_matrix, seed=100)
+    test_x_matrix, test_t_matrix = shuffle_in_unison(training_x_matrix, training_t_matrix, seed=100)
 
     print("Shuffled matricies")
 
-    training_x_matrix = x_matrix[:90]
-    training_t_matrix = t_matrix[:90]
+    #plot_histogram(x_matrix, t_matrix, class_names, feature_names, hist_range=[0, 10], step=0.2)
 
-    test_x_matrix = x_matrix[90:]
-    test_t_matrix = t_matrix[90:]
+    print(training_t_matrix)
+    print(training_x_matrix)
 
-    classes = max(class_array) + 1
+    num_of_iterations = 1000
+
+    classes = len(t_matrix[0])
     features = len(x_matrix[0]) - 1
-
-    num_of_iterations = 10000
-    W_array = [create_W(classes=classes, features=features)]
+    W_array = [np.zeros(shape=(classes, features + 1))]
     for i in range(num_of_iterations):
         W_array = np.append(W_array, train_linear_classifier_iteration(W_array[-1],
                                 training_x_matrix, training_t_matrix, alpha=0.01), axis=0)
@@ -279,13 +419,11 @@ def main():
 
     test_g_matrix = np.transpose(g(W_array[-1], np.transpose(test_x_matrix)))
 
-    plot_confusion_matrix(test_g_matrix, test_t_matrix)
+    plot_confusion_matrix(test_g_matrix, test_t_matrix, class_names)
 
-    plot_incorrect_and_correct(W_array[-1], test_x_matrix, test_t_matrix)
-
+    plot_incorrect_and_correct(W_array[-1], test_x_matrix, test_t_matrix, class_names, feature_names)
 
     W_array = np.array(W_array)
-
     mse_array = []
     error_rate_array = []
     for W in W_array:
