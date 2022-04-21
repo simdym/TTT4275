ea8104cc47094757ebff8ff8d1cac6b9a6023321
Simen Dymbe <simen.dymbe@gmail.com>
2022-04-21 13:57:55 +0200

    add plotting of digits

--- project/digits_project/digits.py
+++ project/digits_project/digits.py
@@ -1,2 +1,20 @@
-import matplotlib.pyplot as plt
+from keras.datasets import mnist
+from matplotlib import pyplot
 
+# loading
+(train_X, train_y), (test_X, test_y) = mnist.load_data()
+
+print(train_y.shape)
+
+# shape of dataset
+print('X_train: ' + str(train_X.shape))
+print('Y_train: ' + str(train_y.shape))
+print('X_test:  ' + str(test_X.shape))
+print('Y_test:  ' + str(test_y.shape))
+
+# plotting
+
+for i in range(9):
+    pyplot.subplot(330 + 1 + i)
+    pyplot.imshow(train_X[i], cmap=pyplot.get_cmap('gray'))
+pyplot.show()
